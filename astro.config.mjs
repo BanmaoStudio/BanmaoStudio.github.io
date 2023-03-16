5883d00b916aa41d1362e0aa366f4ae7d0bf51f5
Maculosa <maculosa@banmao.com>
2023-03-16 16:48:35 +0800

    feat: modify alias name“

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
+import autolinkHeadings from 'remark-autolink-headings';
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  markdown: {
+    remarkPlugins: [],
+    rehypePlugins: [[autolinkHeadings, { behavior: 'prepend' }]],
+    // syntaxHighlight: 'prism'
+  }
 });
